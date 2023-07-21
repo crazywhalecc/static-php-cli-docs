5a97b02b83ad46d8b22fcdce9d779ff28bdff537
crazywhalecc <crazywhalecc@users.noreply.github.com>
2023-07-21 07:36:55 +0000

    deploy: bbfc0e5238eba04832f8b982a1da020920d0ceb7

---
+++ assets/en_faq_index.md.e7212787.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t,U as n}from"./chunks/framework.3f05c8be.js";const y=JSON.parse('{"title":"FAQ","description":"","frontmatter":{},"headers":[],"relativePath":"en/faq/index.md","filePath":"en/faq/index.md"}'),i={name:"en/faq/index.md"},a=n("",20),s=[a];function l(r,c,p,d,h,m){return t(),o("div",null,s)}const f=e(i,[["render",l]]);export{y as __pageData,f as default};
